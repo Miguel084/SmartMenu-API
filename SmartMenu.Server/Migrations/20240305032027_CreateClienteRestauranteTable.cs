5d9c9dea6b6e3b621ce8df7fba615b431b2a2621
Miguel084 <mi084@icloud.com>
2024-03-05 19:40:50 -0300

    feat: - Incremento da pasta de Migrations. - Adicionando alguns elementos na pasta de migrations para o banco de dados.

---
+++ SmartMenu.Server/Migrations/20240305032027_CreateClienteRestauranteTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartMenu.Server.Migrations
+{
+    public partial class CreateClienteRestauranteTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClienteRestaurante",
+                columns: table => new
+                {
+                    CNPJ = table.Column<string>(type: "nvarchar(25)", nullable: false),
+                    RestauranteId = table.Column<int>(type: "int", nullable: false),
+                    RazaoSocial = table.Column<string>(type: "nvarchar(25)", nullable: false), // Ajuste aqui para o tamanho máximo desejado
+                    NomeFantasia = table.Column<string>(type: "nvarchar(25)", nullable: true), // Ajuste aqui para o tamanho máximo desejado
+                    Endereco = table.Column<string>(type: "nvarchar(25)", nullable: true), // Ajuste aqui para o tamanho máximo desejado
+                    Numero = table.Column<string>(type: "nvarchar(10)", nullable: true), // Ajuste aqui para o tamanho máximo desejado
+                    Cep = table.Column<string>(type: "nvarchar(10)", nullable: true), // Ajuste aqui para o tamanho máximo desejado
+                    Email = table.Column<string>(type: "nvarchar(25)", nullable: true), // Ajuste aqui para o tamanho máximo desejado
+                    Telefone = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    Celular = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    InscricaoEstadual = table.Column<string>(type: "nvarchar(20)", nullable: true) // Ajuste aqui para o tamanho máximo desejado
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClienteRestaurante", x => x.CNPJ);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClienteRestaurante");
+        }
+    }
+}
---
+++ SmartMenu.Server/Models/ItemMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartMenu.Server.Models
+{
+    public class ItemMenu
+    {
+        [Key]
+        public int ItemMenuId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        [Required]
+        public decimal Preco { get; set; }
+
+        [Required]
+        public bool Disponibilidade { get; set; }
+
+        [Required]
+        public int CategoriaId { get; set; }
+
+        public string Alergenos { get; set; }
+
+        public string Imagem { get; set; }
+    }
+}
