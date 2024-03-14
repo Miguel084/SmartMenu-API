43b7f9274cc9745232840e6e4f75dea2f4640754
Miguel084 <mi084@icloud.com>
2024-03-14 16:14:16 -0300

    revertendo

---
+++ SmartMenu.Server/Controllers/CardapioController.cs
@@ -0,0 +1,27 @@
+﻿/*
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartMenu.Server.Data;
+using SmartMenu.Server.Models;
+
+namespace SmartMenu.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardapioController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CardapioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Cardapio>> Get()
+        {
+            return await _context.Cardapio.ToListAsync();
+        }
+    }
+}
+*/
