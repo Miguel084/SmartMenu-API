903550f5957904d70687a3d0f938b06174fd0c54
Mariana Parpinelli de Britto <mariana.dev@hotmail.com>
2024-03-23 16:59:12 -0300

    feat: criação da modelo Cliente ClienteModel

---
+++ SmartMenu.Server/Models/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartMenu.Server.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid id;
+
+        [Required]
+        public string cnpj;
+
+        public string inscricaoEnstadual;
+
+        public string razaoSocial;
+
+        [Required]
+        public string email;
+
+        public string telefone;
+
+        public string cep;
+
+        public string endereco;
+
+        public string numero;
+
+        public string bairro;
+
+        public string cidade;
+
+        public string estado;
+
+        [Required]
+        public string senha;
+
+
+    }
+}
