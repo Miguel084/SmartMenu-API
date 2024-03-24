a3aaec8a2fc79be312658f245d14ba35ef90b4c5
Mariana Parpinelli de Britto <mariana.dev@hotmail.com>
2024-03-24 11:39:24 -0300

    feat !: inclusão da API de Clientes, com as rotas de GET /Cliente e POST /Cliente
    Foi necessário recriar o banco de dados algumas vezes. Recomendo apagar toda a pasta de migrações e recriar novamente.

--- SmartMenu.Server/Controllers/ClienteController.cs
+++ SmartMenu.Server/Controllers/ClienteController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using SmartMenu.Server.Data;
 using SmartMenu.Server.Models;
+using System.Runtime.ConstrainedExecution;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,15 +11,10 @@ namespace SmartMenu.Server.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class ClienteController : ControllerBase
+    public class ClienteController(ApplicationDbContext context) : ControllerBase
     {
 
-        private readonly ApplicationDbContext _context;
-
-        public ClienteController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        private readonly ApplicationDbContext _context = context;
 
         [HttpGet]
         public async Task<IEnumerable<Cliente>> Get()
@@ -33,8 +30,19 @@ namespace SmartMenu.Server.Controllers
 
             var cliente = new Cliente();
 
-            cliente.CNPJ = clienteDTO.CNPJ;
-            cliente.RegistroData = new DateOnly();
+                cliente.CadastroCliente(
+                        clienteDTO.CNPJ,
+                        clienteDTO.InstricaoEstadual,
+                        clienteDTO.RazaoSocial,
+                        clienteDTO.NomeFantasia,
+                        clienteDTO.NomeRua,
+                        clienteDTO.NumeroComplemento,
+                        clienteDTO.NomeCidade,
+                        clienteDTO.NomeBairro,
+                        clienteDTO.UF,
+                        clienteDTO.CEP,
+                        clienteDTO.Email
+                    );
 
             _context.Clientes.Add(cliente);
             await _context.SaveChangesAsync();
--- SmartMenu.Server/Models/Cliente.cs
+++ SmartMenu.Server/Models/Cliente.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SmartMenu.Server.Models
 {
-    public class Cliente
+    public class Cliente()
     {
 
         [Key]
@@ -11,27 +11,53 @@ namespace SmartMenu.Server.Models
         public int ClienteId { get; set; }
 
         public string CNPJ { get; set; }
-        //public string InstricaoEstadual { get; set; }
+        public string InstricaoEstadual { get; set; } 
 
-        //public string RazaoSocial { get; set; }
-        //public string NomeFantasia { get; set; }
+        public string RazaoSocial { get; set; } 
+        public string NomeFantasia { get; set; }
 
-        //public string NomeRua { get; set; }
+        public string NomeRua { get; set; }
 
-        //public int NumeroComplemento { get; set; }
+        public int NumeroComplemento { get; set; } 
 
-        //public string NomeCidade { get; set; }
+        public string NomeCidade { get; set; }
 
-        //public string NomeBairro { get; set; }
+        public string NomeBairro { get; set; } 
+        public string UF { get; set; }
 
-        //public string UF { get; set; }
-
-        //public string CEP { get; set; }
-        //public string Email { get; set; }
-
+        public string CEP { get; set; }
+        public string Email { get; set; } 
         public DateOnly RegistroData { get; set; }
 
 
+
+        public void CadastroCliente(
+            string cnpj,
+            string instricaoEstadual,
+            string razaoSocial,
+            string nomeFantasia,
+            string nomeRua,
+            int numeroComplemento,
+            string nomeCidade,
+            string nomeBairro,
+            string uf,
+            string cep,
+            string email
+        )
+        {
+            CNPJ = cnpj;
+            InstricaoEstadual = instricaoEstadual;
+            RazaoSocial = razaoSocial;
+            NomeFantasia = nomeFantasia;
+            NomeRua = nomeRua;
+            NumeroComplemento = numeroComplemento;
+            NomeCidade = nomeCidade;
+            NomeBairro = nomeBairro;
+            UF = uf;
+            CEP = cep;
+            Email = email;
+            RegistroData = DateOnly.FromDateTime(DateTime.Now);
+        }
 
     }
 }
