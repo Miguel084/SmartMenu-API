1c61db9861575671e58e02584e5211e9373a94e4
Miguel084 <134954882+Miguel084@users.noreply.github.com>
2024-03-28 01:27:14 -0300

    arrumando itens importantes
    fix:
    - arrumado logo do site;
    - responsividade;
    - estilização;
    
    feat:
    - adicionado botao de cancelar na tela de cadastro de itens.

--- smartmenu.client/src/Components/Footer.jsx
+++ smartmenu.client/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer style={{ backgroundColor: "#f8f9fa", padding: "20px 0", textAlign: "center" }}>
+        <footer className='fixed-bottom' style={{ backgroundColor: "transparent", textAlign: "center" }}>
             <p style={{ fontSize: "14px", color: "#555" }}> 2024 SmartMenu. Todos os direitos reservados.</p>
         </footer>
     );
--- smartmenu.client/src/Components/Header.jsx
+++ smartmenu.client/src/Components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from '../assets/teste.jpg';
+import logo from '../assets/SmartMenu-Logo.png';
 
 function Header() {
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top" id="navbar">
+        <header className=''>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top " id="navbar">
                 <a className="navbar-brand" href="#">
-                    <img src={logo} alt="Logo do Meu Site" width="100" height="100" className="d-inline-block align-top" />
+                    <img src={logo} alt="Logo do Meu Site" width="100" height="100" className="d-inline-block align-top " />
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,7 +15,7 @@ function Header() {
                 <div className="collapse navbar-collapse align-content-end" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link ponteiro" href="index.html#destino-inicio">Início</a>
+                            <a className="nav-link " href="/">Início</a>
                         </li>
                     </ul>
                 </div>
